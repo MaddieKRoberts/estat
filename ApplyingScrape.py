47f78f59c7078736080452c41a61479677f4cceb
Maddie Kol Roberts <madkroberts2309@gmail.com>
2026-06-19 11:24:40 -0400

    Add files via upload
    fully working code to scrape a file

--- ApplyingScrape.py
+++ ApplyingScrape.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import html2text
 
-
-
 path = r"C:\Users\Kolro\Downloads\scraping_salaries\ss1.html"
 with open(path, "r", encoding='utf-8') as file:
 	cont = file.read()
@@ -14,4 +12,9 @@ tt = soup.find_all('tr')
 
 for t in tt:
 	name = t.find('a')
-	print(html2text.html2text(newname))
+	#print(name.text) if name else print('')
+	try: 
+		print(name.text)  
+
+	except:
+		2+2
